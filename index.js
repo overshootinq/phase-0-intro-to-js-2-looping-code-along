e41b396b8b06db51b8895d8804e3af92fa8823f5
Brennan McCandliss <osmedia1220@gmail.com>
2021-08-24 19:37:48 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let names = ["Craig", "Lewis", "George"];
+const a = "surprise";
+
+
+function writeCards(names, a) {
+    let results = [];
+    for (let i = 0; i < names.length; i++) {
+        results.push(`Thank you, ${names[i]}, for the wonderful ${a} gift!`);
+    }
+    return results;
+}
+
+function countDown(){
+    let i=10;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
